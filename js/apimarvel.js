c147b3806eeafc5bdcdc05311615dd5a02e86d05
jhonrsalcedo <jhonfor@gmail.com>
2019-01-03 02:32:46 -0500

    view data name

--- js/apimarvel.js
+++ js/apimarvel.js
@@ -1,8 +1,9 @@
-var marvel ={
+let marvel ={
     render: function() {
-        var url = 'http://gateway.marvel.com/v1/public/comics?ts=1&apikey=fc51643a4cf9e4d81fd2a8127e2b16cf&hash=adc0117d8a368db1568d7aafe9b836a7';
+        var url = 'http://gateway.marvel.com/v1/public/characters?ts=1&apikey=fc51643a4cf9e4d81fd2a8127e2b16cf&hash=adc0117d8a368db1568d7aafe9b836a7';
        var message = document.getElementById("message"); 
-        
+       var footer = document.getElementById("footer"); 
+       var marvelContainer = document.getElementById("marvel-container");
        $.ajax({
             url: url,
             type: 'GET',
@@ -11,6 +12,30 @@ var marvel ={
             },
             complete: function(){
                 message.innerHTML ="Successfully done!";
+            },
+            success: function(data){
+                footer.innerHTML = data.attributionHTML;
+                var html = "";
+                html += "<div class='row'>";
+
+                for (var i = 0; i < data.data.results.length; i++) {
+                    var datos = data.data.results[i];
+           
+                // data.data.results.forEach(datos =>{
+                //     console.log(datos.name);});
+                    html += `<div class='col-md-3'>
+                   <h3> ${datos.name} </h3>  </div>`;
+
+                    if ((i+1) % 4 == 0) {
+                        html += `</div>
+                        <div class='row'>`;
+                    }
+                }
+             
+                marvelContainer.innerHTML = html;
+            },
+            error: function(){
+                message.innerHTML = "We are sorry!";
             }
        });
     }
